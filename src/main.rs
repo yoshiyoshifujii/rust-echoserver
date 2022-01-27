910a377cf64dcecb5de221a9db910a3144ad30b3
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2022-01-27 22:58:43 +0900

    impl echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+extern crate core;
+
+use core::str;
+use std::{env, thread};
+use std::error::Error;
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let addr = &args[1];
+    echo_server(addr)?;
+    Ok(())
+}
+
+fn echo_server(address: &str) -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind(address)?;
+    loop {
+        let (mut stream, _) = listener.accept()?;
+        thread::spawn(move || {
+            let mut buffer = [0u8; 1024];
+            loop {
+                let nbytes = stream.read(&mut buffer).unwrap();
+                if nbytes == 0 {
+                    return;
+                }
+                println!("{}", str::from_utf8(&buffer[..nbytes]).unwrap());
+                stream.write_all(&buffer[..nbytes]).unwrap();
+            }
+        });
+    }
 }
